b757d0427289a9696e0b1e9a2f329a350338101a
kutya.ms <pluto.koutya@gmail.com>
2017-10-05 19:58:08 +0900

    Initial Commit

---
+++ escaplan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  escaplan
+//
+//  Created by むなかた　しゅん on 2017/10/05.
+//  Copyright © 2017年 Koutya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
